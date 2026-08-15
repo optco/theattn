ff0c8f19b984ed582f3805d9c0ad072673adae1d
Tanvir <132451362+optco@users.noreply.github.com>
2026-08-15 10:30:42 +0200

    Update load.js

--- load.js
+++ load.js
@@ -20,8 +20,8 @@
           position: fixed; 
           top: 6px; 
           left: 6px; 
-          width: 40px;
-          height: 40px;
+          width: 30px;
+          height: 30px;
           background: rgba(255, 255, 255, 0.95); 
           border-radius: 50%; 
           box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
