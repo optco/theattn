570b3d996cbb51e8c7abd981bfb9de236d389569
Tanvir <132451362+optco@users.noreply.github.com>
2026-08-19 15:29:10 +0200

    Update load.js

--- load.js
+++ load.js
@@ -8,7 +8,7 @@
         /* --- Loader --- */
         #pl-indicator {
           position: fixed;
-          bottom: 23px;
+          bottom: 15px;
           left: 0;
           right: 0;
           height: 20px;
